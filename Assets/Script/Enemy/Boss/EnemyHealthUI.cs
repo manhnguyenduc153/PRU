9e939f91ecee4ef587bf4562bda9ef3bf4798b60
manhnguyenduc153 <manhvipyb1@gmail.com>
2025-10-26 12:27:24 +0700

    modify UI player, boss

---
+++ Assets/Script/Enemy/Boss/EnemyHealthUI.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class EnemyHealthUI : MonoBehaviour
+{
+    [Header("UI References")]
+    [SerializeField] private GameObject bossHealthPanel;
+    [SerializeField] private Image healthBarFill;
+    [SerializeField] private Image hudImage;
+    [SerializeField] private TextMeshProUGUI bossNameText;
+
+    [Header("Animation Settings")]
+    [SerializeField] private float fillSpeed = 2f;
+    [SerializeField] private Color healthColor = Color.red;
+
+    private int maxHealth;
+    private int currentHealth;
+    private float targetFillAmount;
+
+    private void Start()
+    {
+        if (healthBarFill != null)
+        {
+            healthBarFill.color = healthColor;
+        }
+
+        // Ẩn UI lúc bắt đầu
+        //if (bossHealthPanel != null)
+        //{
+        //    bossHealthPanel.SetActive(false);
+        //}
+    }
+
+    private void Update()
+    {
+        // Smooth fill animation
+        if (healthBarFill != null)
+        {
+            healthBarFill.fillAmount = Mathf.Lerp(
+                healthBarFill.fillAmount,
+                targetFillAmount,
+                Time.deltaTime * fillSpeed
+            );
+        }
+    }
+
+    public void SetupBossHealth(string name, int current, int max)
+    {
+        maxHealth = max;
+        currentHealth = current;
+        targetFillAmount = 1f;
+
+        if (bossNameText != null)
+        {
+            bossNameText.text = name;
+        }
+
+        if (healthBarFill != null)
+        {
+            healthBarFill.fillAmount = 1f;
+        }
+    }
+
+    public void UpdateHealth(int newHealth)
+    {
+        currentHealth = Mathf.Clamp(newHealth, 0, maxHealth);
+        targetFillAmount = (float)currentHealth / maxHealth;
+    }
+
+    public void ShowBossUI()
+    {
+        if (bossHealthPanel != null)
+        {
+            bossHealthPanel.SetActive(true);
+        }
+    }
+
+    public void HideBossUI()
+    {
+        if (bossHealthPanel != null)
+        {
+            bossHealthPanel.SetActive(false);
+        }
+    }
+}
--- Assets/Script/Enemy/EnemyHealth.cs
+++ Assets/Script/Enemy/EnemyHealth.cs
@@ -5,14 +5,22 @@ using System.Collections;
 
 public class EnemyHealth : MonoBehaviour
 {
+    [Header("Health Settings")]
     [SerializeField] private int startingHealth = 3;
+    
+    [Header("Boss Settings (Optional)")]
+    [SerializeField] private bool isBoss = false;
+    [SerializeField] private string bossName = "Boss Name";
+    
+    [Header("VFX")]
     [SerializeField] private GameObject deathVFXPrefab;
-
+    
     private int currentHealth;
     private EnemyKnockback knockbackScript;
     private Animator animator;
     private Flash flash;
     private LootDropper lootDropper;
+    private EnemyHealthUI healthUI;
 
     private void Awake()
     {
@@ -25,18 +33,35 @@ public class EnemyHealth : MonoBehaviour
         currentHealth = startingHealth;
         knockbackScript = GetComponent<EnemyKnockback>();
         animator = GetComponent<Animator>();
+        
+        // Chỉ tìm và setup UI khi là boss
+        if (isBoss)
+        {
+            healthUI = FindObjectOfType<EnemyHealthUI>();
+            if (healthUI != null)
+            {
+                healthUI.SetupBossHealth(bossName, currentHealth, startingHealth);
+                healthUI.ShowBossUI();
+            }
+        }
     }
 
     public void TakeDamage(int damage, Transform attacker)
     {
         currentHealth -= damage;
         Debug.Log(currentHealth);
-
+        
+        // Chỉ update UI khi là boss
+        if (isBoss && healthUI != null)
+        {
+            healthUI.UpdateHealth(currentHealth);
+        }
+        
         if (flash != null)
         {
             StartCoroutine(flash.FlashRoutine());
         }
-
+        
         if (knockbackScript != null && attacker != null)
         {
             knockbackScript.ApplyKnockback(attacker);
@@ -47,12 +72,18 @@ public class EnemyHealth : MonoBehaviour
     {
         currentHealth -= damage;
         Debug.Log(currentHealth);
-
+        
+        // Chỉ update UI khi là boss
+        if (isBoss && healthUI != null)
+        {
+            healthUI.UpdateHealth(currentHealth);
+        }
+        
         if (flash != null)
         {
             StartCoroutine(flash.FlashRoutine());
         }
-
+        
         if (knockbackScript != null)
         {
             knockbackScript.ApplyKnockback(knockbackDirection);
@@ -63,7 +94,13 @@ public class EnemyHealth : MonoBehaviour
     {
         currentHealth -= damage;
         Debug.Log(currentHealth);
-
+        
+        // Chỉ update UI khi là boss
+        if (isBoss && healthUI != null)
+        {
+            healthUI.UpdateHealth(currentHealth);
+        }
+        
         if (flash != null)
         {
             StartCoroutine(flash.FlashRoutine());
@@ -74,18 +111,24 @@ public class EnemyHealth : MonoBehaviour
     {
         if (currentHealth <= 0)
         {
+            // Ẩn UI nếu là boss
+            if (isBoss && healthUI != null)
+            {
+                healthUI.HideBossUI();
+            }
+            
             // Drop loot
             if (lootDropper != null)
             {
                 lootDropper.DropLoot();
             }
-
+            
             // VFX
             if (deathVFXPrefab != null)
             {
                 Instantiate(deathVFXPrefab, transform.position, Quaternion.identity);
             }
-
+            
             Destroy(gameObject);
         }
     }
@@ -93,15 +136,12 @@ public class EnemyHealth : MonoBehaviour
     private IEnumerator Die()
     {
         GetComponent<Collider2D>().enabled = false;
-
         var movement = GetComponent<MonoBehaviour>();
         if (movement != null)
         {
             movement.enabled = false;
         }
-
         yield return new WaitForSeconds(1f);
-
         Destroy(gameObject);
     }
 }
