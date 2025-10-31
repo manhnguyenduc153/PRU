21337c6aa1de5b92e5947d08b75373149ebec7c1
Tai Nguyen Van <buiblue123@gmail.com>
2025-11-01 01:07:33 +0700

    feat: base menu instruction v2

--- Assets/Script/MainMenuController.cs
+++ Assets/Script/MainMenuController.cs
@@ -4,25 +4,40 @@ using UnityEngine.SceneManagement;
 public class MainMenuController : MonoBehaviour
 {
     public GameObject instructionPanel;
+    public CanvasGroup instructionGroup;
 
     public void StartGame()
     {
         SceneManager.LoadScene("Scene1");
     }
 
-    public void ShowInstructions()
-    {
-        instructionPanel.SetActive(true);
-    }
+    // public void ShowInstructions()
+    // {
+    //     instructionPanel.SetActive(true);
+    // }
 
-    public void HideInstructions()
-    {
-        instructionPanel.SetActive(false);
-    }
+    // public void HideInstructions()
+    // {
+    //     instructionPanel.SetActive(false);
+    // }
 
     public void QuitGame()
     {
         Application.Quit();
         Debug.Log("Game exited."); // chỉ thấy khi chạy trong editor
+    }
+
+    public void ShowInstructions()
+    {
+        instructionGroup.alpha = 1;
+        instructionGroup.interactable = true;
+        instructionGroup.blocksRaycasts = true;
+    }
+
+    public void HideInstructions()
+    {
+        instructionGroup.alpha = 0;
+        instructionGroup.interactable = false;
+        instructionGroup.blocksRaycasts = false;
     }
 }
